8df57705cf74831fc63f570f87cfbc1f4d1afde3
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-05 17:22:29 -0700

    master

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -44,16 +44,6 @@ smallest entry with entry n-1 (so do nothing).
 #include <iostream>
 using namespace std;
 
-
-void swap(int x, int y) {
-
-	int temp = x;
-	x = y;
-	y = temp;
-
-
-}
-
 int main() {
 
 	int n, input;
