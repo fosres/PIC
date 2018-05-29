eae3c7795a55b2063f0d25da10b60d96b9274665
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-28 17:40:22 -0700

    master

--- card/hand.cpp
+++ card/hand.cpp
@@ -5,28 +5,28 @@ bool hand::operator<(hand rhs)
 {
 
 	if ( (c1.getNum() == c2.getNum()) )
-		return false;
+		return 0;
 	else if ( c1.getSuit() == c2.getSuit())
 	{
 		if (rhs.c1.getSuit() != rhs.c2.getSuit())
-			return false;
+			return 0;
 		else //within the same kind of hands
 		{
 			if (c1.getNum() > rhs.c1.getNum()) // see if the c1 of lhs is greater or equal to
-				return false;
+				return 0;
 			else if (c1.getNum() == rhs.c1.getNum())
 			{
 				if (c2.getNum() >= rhs.c2.getNum())
-					return false;
+					return 0;
 				else // c2 of lhs ACTUALLY IS LESS, but c1s of lhs and rhs are same
-					return true;
+					return 1;
 
 			}
 				
 		}
 	}
 	// all above fails, two hands are equal
-		return false;
+		return 0;
 
 }
 
@@ -38,7 +38,7 @@ bool hand::operator>=(hand rhs)
 
 bool hand::operator==(hand rhs)
 {
-	return (( *this <= rhs ) && ( rhs <= *this));
+	return (!( *this < rhs ) && !( rhs < *this));
 
 }
 
@@ -47,7 +47,6 @@ bool hand::operator<=(hand rhs)
 	return ((*this < rhs) || (*this == rhs));	
 
 }
-
 bool hand::operator>(hand rhs)
 {
 	return !(*this <= rhs);	
