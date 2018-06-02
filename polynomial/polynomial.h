7555207728683880ffc67eb3d1ac345f9b896619
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-02 14:53:45 -0700

    master

--- polynomial/polynomial.h
+++ polynomial/polynomial.h
@@ -1,6 +1,7 @@
 #ifndef __POLYNOMIAL_H__
 #define __POLYNOMIAL_H__
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <vector>
 using std::vector;
@@ -33,6 +34,18 @@ int polynomial::degree()
 
 }
 
+int polynomial::nonzeroTerms()
+{
+	
+	int num_zero_terms = 0;
+	for ( int i = 0; i < v.capacity(); i++)
+		if ( v[i] != 0)
+			num_zero_terms++;
+
+	return num_zero_terms;
+
+}
+
 void polynomial::setCoeff(int deg, double c)
 {
 
@@ -55,8 +68,66 @@ void polynomial::setCoeff(int deg, double c)
 
 }
 
-polynomial operator+(double c, polynomial p);
-polynomial operator*(double c, polynomial p);
+double polynomial::getCoeff(int deg)
+{
+	assert (deg >= 0);
+
+	if ( (deg > v.capacity()-1) )
+		return 0;
+	else
+		return v[deg];
+
+}
+
+  double polynomial::operator()(double x)
+{
+
+	double ans = 0;
+
+	for (int i = 0; i < v.capacity(); i++)
+		ans += getCoeff(i)*pow(x,i);
+
+	return ans;
+}
+
+
+// just add c to 0th element of vector
+polynomial operator+(double c, polynomial p)
+{
+	p.setCoeff(0,p.getCoeff(0)+c);	
+	
+	return p;
+
+}
+
+polynomial operator*(double c, polynomial p)
+{
+	p.setCoeff(0,p.getCoeff(0)*c);	
+	
+	return p;
+
+}
+
+  polynomial operator+(polynomial p)
+{
+	polynomial sum;
+	if ( *this.capacity() > p.capacity() )
+		sum.resize(this.capacity());
+	else // *this.capacity() <= p.capacity()
+		sum.resize(p.capacity());
+
+	for ( int i = 0; i < sum.capacity(); i++)
+			
+
+}
+  polynomial& operator+=(polynomial p);
+  polynomial operator-();
+  polynomial operator-(polynomial p);
+  polynomial& operator-=(polynomial p);
+  polynomial operator*(polynomial p);
+  polynomial& operator*=(polynomial p);
+
+
 std::ostream& operator<<(std::ostream& s, polynomial p);
 
 
